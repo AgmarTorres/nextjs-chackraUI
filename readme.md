1fd84e0637e9fff1109e2f4a1cc8fcf19567d26e
Agmar Torres <agmartorres@gmail.com>
2021-05-11 09:33:20 -0300

    feat: login page with inputs and styles based Chakra-ui

--- readme.md
+++ readme.md
@@ -5,3 +5,7 @@ yarn create next-app 04-dashgo
 yarn add typescript @types/react @types/node -D
 
 yarn add @chakra-ui/react @chakra-ui/core @emotion/react @emotion/styled framer-motion
+
+split html attributs
+
+padding in the chakra +/- 2rem \*4 caso necessite de pixels e divide por 4 caso queira em rem
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,55 @@
+import {
+  Button,
+  Flex,
+  FormLabel,
+  Input,
+  Stack,
+  FormControl,
+} from '@chakra-ui/react';
+
 export default function Home() {
-  return <h1>dashboard</h1>;
+  return (
+    <Flex w='100vw' h='100vh' align='center' justify='center'>
+      <Flex
+        as='form'
+        width='100%'
+        maxW={360}
+        bg='gray.800'
+        p='8'
+        borderRadius='8'
+        flexDir='column'
+      >
+        <Stack spacing='4'>
+          <FormControl>
+            <FormLabel htmlFor='email'>E-mail:</FormLabel>
+
+            <Input
+              name='email'
+              type='email'
+              focusBorderColor='pink.500'
+              bgColor='gray.900'
+              variant='filled'
+              _hover={{ bgColor: 'gray.900' }}
+              size='lg'
+            />
+          </FormControl>
+          <FormControl>
+            <FormLabel htmlFor='password'>Senha:</FormLabel>
+            <Input
+              name='password'
+              type='password'
+              focusBorderColor='pink.500'
+              bgColor='gray.900'
+              variant='filled'
+              _hover={{ bgColor: 'gray.900' }}
+              size='lg'
+            />
+          </FormControl>
+        </Stack>
+        <Button type='submit' mt='6' size='lg' colorScheme='pink'>
+          Entrar
+        </Button>
+      </Flex>
+    </Flex>
+  );
 }
